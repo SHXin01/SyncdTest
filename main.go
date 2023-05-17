c353e3884ed283a15bb0a594feb9ddae97289e2b
shanghongxin <shanghongxinvip@163.com>
2023-05-17 14:05:23 +0800

    同步

--- main.go
+++ main.go
@@ -5,11 +5,7 @@
 // -------------------------------------------
 package main
 
-<<<<<<< HEAD
 const version = "V 1.0.2"
-=======
-const version = "V 1.0.1"
->>>>>>> origin/main
 
 func main() {
 	println(version)

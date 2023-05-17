025491f2a0921f85a59a9c36c0508100cf719ea3
shanghongxin <shanghongxinvip@163.com>
2023-05-17 11:20:36 +0800

    同步

---
+++ main.go
@@ -0,0 +1,12 @@
+// Package SyncdTest -----------------------------
+// @file      : main.go
+// @author    : SHXin
+// @time      : 2023/5/17 11:20
+// -------------------------------------------
+package main
+
+const version = "V 1.0.1"
+
+func main() {
+	println(version)
+}
